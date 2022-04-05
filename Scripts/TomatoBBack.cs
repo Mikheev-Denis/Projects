1b60a62b0a62c6e9478c8cefe8a660492dddb417
Mikheev-Denis <59222498+Mikheev-Denis@users.noreply.github.com>
2022-04-05 13:24:55 +0300

    Add files via upload
    BBQ Destroyers catapult code

---
+++ Scripts/TomatoBBack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TomatoBBack : MonoBehaviour
+{
+    public GameObject fxJoint;
+    public Rigidbody tomato;
+    public FixedJoint joint;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            fxJoint.SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            fxJoint.SetActive(true);
+            joint.connectedBody = tomato;
+            
+        }
+    }
+}
